bdebcc05b4456e8526e65f01c9fa79e74e30bb7a
Lam Chan <lamchakchan@gmail.com>
2012-08-07 10:39:48 -0500

    added backward compatibility

--- Enumeration.cs
+++ Enumeration.cs
@@ -7,6 +7,27 @@ namespace Headspring
 {
     [Serializable]
     [DebuggerDisplay("{DisplayName} - {Value}")]
+    public abstract class Enumeration<TEnumeration> : EnumerationGeneric<TEnumeration, int>
+        where TEnumeration : Enumeration<TEnumeration>
+    {
+        protected Enumeration(int value, string displayName)
+            : base(value, displayName)
+        {
+        }
+
+        public static TEnumeration FromInt32(int value)
+        {
+            return FromValue(value);
+        }
+
+        public static bool TryFromInt32(int listItemValue, out TEnumeration result)
+        {
+            return TryParse(listItemValue, out result);
+        }
+    }
+
+    [Serializable]
+    [DebuggerDisplay("{DisplayName} - {Value}")]
     public abstract class Enumeration<TEnumeration, TValue> : IComparable<TEnumeration>, IEquatable<TEnumeration>
         where TEnumeration : Enumeration<TEnumeration, TValue>
         where TValue : IComparable
