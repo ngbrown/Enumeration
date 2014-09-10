c15ccde8d7c13ab8d3c06c5cd9c2ffd320f2baf2
Chris Missal <chris.missal@gmail.com>
2014-09-10 00:23:34 -0500

    add sort tests

---
+++ Enumeration/Tests/SortTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests
+{
+    using System;
+    using System.Linq;
+    using Shouldly;
+
+    public class SortTests
+    {
+        public void Enumerations_should_sort_by_Value_by_default()
+        {
+            var states = new [] { State.Stopping, State.Off, State.Starting, State.Busy, State.Waiting };
+
+            Array.Sort(states);
+
+            states.ElementAt(0).ShouldBe(State.Off);
+            states.ElementAt(1).ShouldBe(State.Starting);
+            states.ElementAt(2).ShouldBe(State.Waiting);
+            states.ElementAt(3).ShouldBe(State.Busy);
+            states.ElementAt(4).ShouldBe(State.Stopping);
+        }
+    }
+}
