95467b69a6075ef9256ed4dfff6cb905608acfd9
Chris Missal <chris.missal@gmail.com>
2014-09-09 23:51:23 -0500

    add equality tests

---
+++ Enumeration/Tests/Color.cs
@@ -0,0 +1,14 @@
+﻿namespace Tests
+{
+    using Headspring;
+
+    public class Color : Enumeration<Color>
+    {
+        public static readonly Color Red = new Color(1, "Red");
+        public static readonly Color Blue = new Color(2, "Blue");
+
+        private Color(int value, string displayName) : base(value, displayName)
+        {
+        }
+    }
+}
